873622a36786182a3a032e2a86bde0b4c17f9dcf
Geolynx Saemane <gsaemane@genisys.com.sb>
2026-01-31 19:33:01 +1100

    update landing page add new sections

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'rebays-website.s3.ap-southeast-2.amazonaws.com', // Replace with your actual S3 URL
-        port: '',
-        pathname: '/**',
-      },
-    ],
+    unoptimized:true,
+    // remotePatterns: [
+    //   {
+    //     protocol: 'https',
+    //     hostname: 'rebays-website.s3.ap-southeast-2.amazonaws.com', // Replace with your actual S3 URL
+    //     port: '',
+    //     pathname: '/**',
+    //   },
+    // ],
   },
 };
 
